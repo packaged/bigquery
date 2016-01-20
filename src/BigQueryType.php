b3a8d2afec3a2e64eb30d55da9aba3b9983e032e
richard.gooding <richard.gooding@justdevelop.it>
2016-01-20 14:04:47 +0000

    Added class containing type constants

---
+++ src/BigQueryType.php
@@ -0,0 +1,18 @@
+<?php
+namespace Packaged\BigQuery;
+
+class BigQueryType
+{
+  /** 2 MB UTF-8 encoded string */
+  const STRING = 'string';
+  /** 64-bit signed integer */
+  const INTEGER = 'integer';
+  /** Double-precision floating-point format */
+  const FLOAT = 'float';
+  /** true or false (case-insensitive) */
+  const BOOLEAN = 'boolean';
+  /** A collection of one or more other fields */
+  const RECORD = 'record';
+  /** YYYY-MM-DD HH:MM:SS or UNIX timestamp with up to 6 decimal places */
+  const TIMESTAMP = 'timestamp';
+}
